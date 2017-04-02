0b4b453aced094ef52604c4b898d95b901a4b12c
ms-dev <dev@mingsoft.net>
2017-04-02 12:09:18 +0800

    会员模块自定义页面数据更新

--- doc/db-mcms-open-4.6.0.sql
+++ doc/db-mcms-open-4.6.0.sql
@@ -1040,10 +1040,10 @@ CREATE TABLE `model_template` (
 -- ----------------------------
 -- Records of model_template
 -- ----------------------------
-INSERT INTO `model_template` VALUES ('1', '0', '1', 'register.htm', '注册', 'register');
-INSERT INTO `model_template` VALUES ('2', '0', '1', 'login.htm', '登录', 'login');
-INSERT INTO `model_template` VALUES ('3', '0', '1', 'userCenter.htm', '个人中心', 'people/userCenter');
-INSERT INTO `model_template` VALUES ('4', '0', '1', 'changePassWord.htm', '修改密码', 'changePassWord');
+INSERT INTO `model_template` VALUES ('1', '0', '1', 'people/register.htm', '注册', 'register');
+INSERT INTO `model_template` VALUES ('2', '0', '1', 'people/login.htm', '登录', 'login');
+INSERT INTO `model_template` VALUES ('3', '0', '1', 'people/center.htm', '个人中心', 'people/userCenter');
+INSERT INTO `model_template` VALUES ('4', '0', '1', 'people/forget-password.htm', '修改密码', 'changePassWord');
 
 -- ----------------------------
 -- Table structure for people
