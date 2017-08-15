b1ce6c1b3fc0de3ef11c7f3ec4db5b3326c36adf
ms-dev <dev@mingsoft.net>
2017-08-15 10:24:50 +0800

    数据优化，支持mysql5.7之前的版本

--- doc/patch-4.6.0.sql
+++ doc/patch-4.6.0.sql
@@ -19,10 +19,12 @@ ALTER TABLE mdiy_page CHANGE mt_appID page_app_id  int(11) DEFAULT NULL COMMENT 
 ALTER TABLE mdiy_page CHANGE mt_path page_path  varchar(255) DEFAULT NULL COMMENT '自定义页面绑定模板的路径';
 ALTER TABLE mdiy_page CHANGE mt_title page_title varchar(255) DEFAULT NULL COMMENT '自定义页面标题';
 ALTER TABLE mdiy_page CHANGE mt_key page_key varchar(255) DEFAULT NULL COMMENT '自定义页面访问路径';
-ALTER TABLE mdiy_page 
-RENAME INDEX `mt_key` TO `page_key`,
-RENAME INDEX `mt_appID` TO `page_app_id`,
-RENAME INDEX `mt_modelID` TO `page_model_id`;
+ALTER TABLE mdiy_page DROP INDEX mt_key;
+ALTER TABLE mdiy_page ADD INDEX index_page_key(page_key);
+ALTER TABLE mdiy_page DROP INDEX mt_appID;
+ALTER TABLE mdiy_page ADD INDEX index_page_app_id(page_app_id);
+ALTER TABLE mdiy_page DROP INDEX mt_modelID;
+ALTER TABLE mdiy_page add INDEX index_page_model_id(page_model_id);
 ALTER TABLE `mdiy_content_model` ADD COLUMN `CM_APP_ID` int(0) COMMENT '应用编号' AFTER `CM_MODEL_ID`;
 ALTER TABLE `mdiy_content_model` CHANGE CM_TIPSNAME CM_TIPS_NAME varchar(30) NOT NULL COMMENT '表单提示文字';
 ALTER TABLE `mdiy_content_model` CHANGE CM_TABLENAME CM_TABLE_NAME  varchar(20) NOT NULL COMMENT '表单名称';
