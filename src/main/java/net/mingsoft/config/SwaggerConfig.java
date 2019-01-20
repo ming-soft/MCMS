60211138002d717112bfd2e04f2567fa42c1f3b4
ms group dev <dev@mingsoft.net>
2019-01-20 12:18:44 +0800

    配置更新

--- src/main/java/net/mingsoft/config/SwaggerConfig.java
+++ src/main/java/net/mingsoft/config/SwaggerConfig.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2018-2020 stylefeng & fengshuonan (https://gitee.com/stylefeng)
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.mingsoft.config;
 
 import org.springframework.context.annotation.Bean;
@@ -27,19 +12,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * swagger配置类
- *
- * @author fengshuonan
- * @date 2017年6月1日19:42:59
- */
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				// 自行修改为自己的包路径
 				.apis(RequestHandlerSelectors.basePackage("net.mingsoft")).paths(PathSelectors.any())
 				.build();
 	}
--- src/main/java/net/mingsoft/config/SwaggerSuffixAspect.java
+++ src/main/java/net/mingsoft/config/SwaggerSuffixAspect.java
@@ -12,31 +12,24 @@ import org.springframework.stereotype.Component;
 import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 
-/**
- * 将接口url中追加模式后缀.do
- * @author impler
- * @date 2017年9月30日
- */
 @Aspect
 @EnableAspectJAutoProxy
 @Component
 public class SwaggerSuffixAspect {
-    
-    @AfterReturning(pointcut="execution(public io.swagger.models.Swagger springfox.documentation.swagger2.mappers.ServiceModelToSwagger2MapperImpl.mapDocumentation(..))",
-            returning="swagger")
-    public void doBeforeBussinessCheck(Swagger swagger){
-        Map<String, Path> paths = swagger.getPaths();
-        if(null != paths){
-            Map<String, Path> newPaths = new HashMap<String, Path>(paths);
-            paths.clear();
-            Iterator<String> it = newPaths.keySet().iterator();
-            while(it.hasNext()){
-                String oldKey = it.next();
-                // 添加模式后缀 .do
-                String newKey = oldKey  + ".do";
-                paths.put(newKey, newPaths.get(oldKey));
-            }
-            newPaths = null;
-        }
-    }
+
+	@AfterReturning(pointcut = "execution(public io.swagger.models.Swagger springfox.documentation.swagger2.mappers.ServiceModelToSwagger2MapperImpl.mapDocumentation(..))", returning = "swagger")
+	public void doBeforeBussinessCheck(Swagger swagger) {
+		Map<String, Path> paths = swagger.getPaths();
+		if (null != paths) {
+			Map<String, Path> newPaths = new HashMap<String, Path>(paths);
+			paths.clear();
+			Iterator<String> it = newPaths.keySet().iterator();
+			while (it.hasNext()) {
+				String oldKey = it.next();
+				String newKey = oldKey + ".do";
+				paths.put(newKey, newPaths.get(oldKey));
+			}
+			newPaths = null;
+		}
+	}
 }
