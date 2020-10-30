6b271adb968dd37f2e332be2bf82d3dd592f9cb5
sgjj <995959152@qq.com>
2020-10-30 10:34:53 +0800

    恢复代码

--- src/main/java/net/mingsoft/config/WebConfig.java
+++ src/main/java/net/mingsoft/config/WebConfig.java
@@ -1,25 +1,22 @@
 package net.mingsoft.config;
 
-import java.io.File;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.spring.stat.BeanTypeAutoProxyCreator;
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.baomidou.mybatisplus.extension.toolkit.JdbcUtils;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.mingsoft.basic.filter.XSSEscapeFilter;
+import net.mingsoft.basic.interceptor.ActionInterceptor;
+import net.mingsoft.handler.AppHandler;
 import net.mingsoft.interceptor.DMInnerInterceptor;
 import net.mingsoft.interceptor.MysqlInnerInterceptor;
-import org.springframework.aop.Advisor;
-import net.mingsoft.basic.filter.XSSEscapeFilter;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -27,23 +24,18 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import com.alibaba.druid.support.spring.stat.BeanTypeAutoProxyCreator;
-import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.mingsoft.basic.interceptor.ActionInterceptor;
-import net.mingsoft.basic.util.BasicUtil;
-
 import javax.sql.DataSource;
+import java.io.File;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -65,11 +57,15 @@ public class WebConfig implements WebMvcConfigurer {
 	public ActionInterceptor actionInterceptor() {
 		return new ActionInterceptor();
 	}
-
+	@Bean
+	public AppHandler appHandler() {
+		return new AppHandler();
+	}
 
 	@Bean
 	public MybatisPlusInterceptor mybatisPlusInterceptor(DataSource dataSource) {
 		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(appHandler()));
 		try {
 			//mysql 添加转换sql
 			DbType dbType = JdbcUtils.getDbType(dataSource.getConnection().getMetaData().getURL());
@@ -83,6 +79,10 @@ public class WebConfig implements WebMvcConfigurer {
 		}
 
 		return interceptor;
+	}
+	@Bean
+	public ConfigurationCustomizer configurationCustomizer() {
+		return configuration -> configuration.setUseDeprecatedExecutor(false);
 	}
 	/**
 	 * 增加对rest api鉴权的spring mvc拦截器
