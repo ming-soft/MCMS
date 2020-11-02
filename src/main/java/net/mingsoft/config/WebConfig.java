d68b4dd9e027272dd79f36eecb8fb50f2308df38
sgjj <995959152@qq.com>
2020-11-02 16:46:41 +0800

    调整配置

--- src/main/java/net/mingsoft/config/WebConfig.java
+++ src/main/java/net/mingsoft/config/WebConfig.java
@@ -60,26 +60,6 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	@Bean
-	public MybatisPlusInterceptor mybatisPlusInterceptor(DataSource dataSource,@Autowired(required = false) TenantLineHandler tenantLineHandler) {
-		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-		if(tenantLineHandler!=null){
-			interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(tenantLineHandler));
-		}
-		try {
-			//mysql 添加转换sql
-			DbType dbType = JdbcUtils.getDbType(dataSource.getConnection().getMetaData().getURL());
-			if(DbType.MYSQL==dbType){
-				interceptor.addInnerInterceptor(new MysqlInnerInterceptor());
-			}else if(DbType.DM==dbType){
-				interceptor.addInnerInterceptor( new DMInnerInterceptor());
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return interceptor;
-	}
-	@Bean
 	public ConfigurationCustomizer configurationCustomizer() {
 		return configuration -> configuration.setUseDeprecatedExecutor(false);
 	}
