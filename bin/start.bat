954b508174308a03d5fc31322537e0cf80cfde4e
msgroup <dev@mingsoft.net>
2025-05-14 10:16:55 +0800

    fix：规范

--- bin/start.bat
+++ bin/start.bat
@@ -1,5 +1,4 @@
 @echo off
-
 start javaw -jar -Dfile.encoding=utf-8 -Duser.timezone=GMT+08 -Duser.language=zh -Duser.region=CN ./ms-mcms.jar
 :: > boot.log 2>&1
 exit
--- bin/stop.bat
+++ bin/stop.bat
@@ -1,5 +1,3 @@
 @echo off
-
 taskkill -f -t -im javaw.exe
-
 exit
