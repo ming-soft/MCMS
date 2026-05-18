88666098bdc5ac93af53bdbc3078c668c8238b0c
msgroup <dev@mingsoft.net>
2026-05-18 14:10:25 +0800

    remove(template): 删除关于页面、案例列表页、联系页面及相关资源文件
    - 移除 about.htm 关于页面模板文件
    - 移除 caselist.htm 案例列表页面模板文件
    - 移除 contact.htm 联系页面模板文件
    - 移除 css/anim.css 动画样式表文件
    - 移除 js/anim.js 动画JavaScript文件
    - 移除 js/basic.js 基础功能JavaScript文件
    - 移除 js/048c11927f4c4b8a8811578543381d58.js 百度地图脚本文件
    - 移除 css/css.css 样式表文件中的大量CSS规则
    - 清理页面中引用的地图组件和动画组件相关代码

--- src/main/webapp/template/1/default/js/048c11927f4c4b8a8811578543381d58.js
+++
@@ -1,1 +0,0 @@
-(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=1.4&ak=&services=&t=20180102095227"></script>');})();
